3e0b202309383f4448b7fd2e7e916bd1efc4be9d
Riya Halbhavi <riyahal.dev@gmail.com>
2025-01-24 19:26:50 +0530

    Add files via upload

---
+++ Input and Output Functions/Input and Output Functions.c
@@ -0,0 +1,14 @@
+// 
+
+#include <stdio.h>
+int main() {
+    char name[10];
+    int age;
+    printf("Enter your name\n");
+    scanf("%s", &name);
+    printf("Enter your age\n");
+    scanf("%d", &age);
+    printf("Name:%s\n", name);
+    printf("Age:%d", age);
+    return 0;
+}
---
+++ Variables/Variables.c
@@ -0,0 +1,9 @@
+// basic.c
+
+#include <stdio.h>
+int main() {
+    char name[] = "Riya";
+    int age = 31;
+    printf("%s\n", name);
+    printf("Age: %d\n", age);
+}
